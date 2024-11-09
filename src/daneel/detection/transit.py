add1370fc279288683b7cc18fdea68774d457e74
FrancescoBrevi01 <francesco.brevi@studenti.unipd.it>
2024-11-09 12:20:20 +0100

    update assignment1 task F, first plot of light curve

--- src/daneel/detection/transit.py
+++ src/daneel/detection/transit.py
@@ -2,23 +2,29 @@ import batman
 import numpy as np
 import matplotlib.pyplot as plt
 
-# params = batman.TransitParams()
-# params.t0 = 0.                              #time of inferior conjunction
-# params.per = 3.21                           #orbital period
-# params.rp = 0.7                             #planet radius (in units of stellar radii)
-# params.a = 0.0384 * 149597870707/6957e8     #semi-major axis (in units of stellar radii)    1 AU = 149597870707 m ; 1 Rsun = 6957e8 m
-# params.inc = 90.                            #orbital inclination (in degrees)
-# params.ecc = 0.                             #eccentricity
-# params.w = 90.                              #longitude of periastron (in degrees)
-# params.u = [0.1, 0.3]                       #limb darkening coefficients [u1, u2]
-# params.limb_dark = "quadratic"              #limb darkening model
+data_light_curve = np.genfromtxt('ExoCTK_results.txt',usecols=[8,10],skip_header=2)
 
-# t = np.linspace(-1.7, 1.7, 100)
-# m = batman.TransitModel(params, t)          #initializes model
-# flux = m.light_curve(params)                #calculates light curve
+c1 = np.mean(data_light_curve[:,0])
+c2 = np.mean(data_light_curve[:,1])
 
-# # plot
-# plt.plot(t, flux)
-# plt.xlabel("Time from central transit")
-# plt.ylabel("Relative flux")
+params = batman.TransitParams()
+params.t0 = 0.                            #  #time of inferior conjunction
+params.per = 3.21                           #orbital period
+params.rp = 0.7                             #planet radius (in units of stellar radii)
+params.a = 0.0384 * 149597870707/6957e8     #semi-major axis (in units of stellar radii)    1 AU = 149597870707 m ; 1 Rsun = 6957e8 m
+params.inc = 90.                          #  #orbital inclination (in degrees)
+params.ecc = 0.                           #  #eccentricity
+params.w = 90.                            #  #longitude of periastron (in degrees)
+params.u = [c1, c2]                         #limb darkening coefficients [u1, u2]
+params.limb_dark = "quadratic"              #limb darkening model
+
+t = np.linspace(-1.7, 1.7, 100)
+m = batman.TransitModel(params, t)          #initializes model
+flux = m.light_curve(params)                #calculates light curve
+
+# plot
+plt.plot(t, flux)
+plt.xlabel("Time from central transit")
+plt.ylabel("Relative flux")
+plt.savefig('HAT-P-12b_assignment1_taskF.png')
 # plt.show()
